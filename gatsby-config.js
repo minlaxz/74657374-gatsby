b2224d8cff59c1b7e10ffb2b67ee0f5168c7ea0b
minlaxz <minminlaxz@gmail.com>
2021-10-26 12:22:11 +0630

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://minlaxz.github.io/74657374-gatsby",
     title: "74657374-gatsby",
   },
   plugins: ["gatsby-plugin-styled-components"],
