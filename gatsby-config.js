3417e551c0d7b4d785efeaad534c9767f880c679
minlaxz <minminlaxz@gmail.com>
2021-10-26 22:03:07 +0630

    Gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     siteUrl: "https://minlaxz.github.io/74657374-gatsby",
     title: "74657374-gatsby",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 };
