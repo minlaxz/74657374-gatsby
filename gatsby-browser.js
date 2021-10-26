709139e5e8fa560e260c1cc454f905159053066b
minlaxz <minminlaxz@gmail.com>
2021-10-26 23:53:07 +0630

    code mdx

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+import "prismjs/themes/prism-solarizedlight.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/command-line/prism-command-line.css";
+import "./src/styles/prism.css";
+import "./src/styles/lines.css";
+import "./src/styles/shell.css";
