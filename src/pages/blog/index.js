426922a9dd55548a21668538ecc1a9ea8811f66a
minlaxz <minminlaxz@gmail.com>
2021-10-27 12:56:37 +0630

    dynamic page routing and rendering

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,20 +1,19 @@
 import * as React from 'react'
-import { graphql } from 'gatsby'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
-import Layout from '../components/layout'
+import { graphql, Link } from 'gatsby'
+import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
     return (
         <Layout pageTitle="My Blog Posts">
             {
                 data.allMdx.nodes.map((node) => (
-                    <article key={node.id} style={{border: "1px dotted #663399", margin:"0.5em", padding:"1em"}}>
-                        <h2>{node.frontmatter.title}</h2>
+                    <article key={node.id} style={{ border: "1px dotted #663399", margin: "0.5em", padding: "1em" }}>
+                        <h2>
+                            <Link to={`/blog/${node.slug}`}>
+                                {node.frontmatter.title}
+                            </Link>
+                        </h2>
                         <p>Posted: {node.frontmatter.date}</p>
-                        <MDXRenderer>
-                            {node.body}
-                        </MDXRenderer>
-                        {/* <hr/> */}
                     </article>
                 ))
             }
@@ -31,7 +30,7 @@ export const query = graphql`
                 title
             }
         id
-        body
+        slug
         }
     }
 }
